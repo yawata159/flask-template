932d600ec8b9607b11be9b901266880a1ec6c589
AWinnerIsYou <seijiyawata@gmail.com>
2016-09-20 19:37:58 -0400

    Expand hello.py

--- hello.py
+++ hello.py
@@ -1,26 +1,27 @@
 # Import Flask class from flask module
 from flask import Flask
-
+import random
 # New Flask object 
 app = Flask(__name__)
 
 # Associate route to fxn
 @app.route("/")
 def hola():
-    return "Hello!"
-
+    return '''<h1>Welcome to the Main Page!</h1><hr>
+<a href=about>About</a><br>
+<a href=random>Random</a>
+'''
 
 @app.route("/about")
 def about():
-    return "This is the about page"
+    return '''Made with Flask<hr>
+<a href=..>Go Back</a>'''
 
-@app.route("/info")
+@app.route("/random")
 def info():
-    return "This is the info page."
-
-@app.route("/credits")
-def credits():
-    return "This is the credits page."
+    dice = random.randin(1,6)
+    return "You rolled a " + str(dice) + '''<hr>
+<a href=..>Go Back</a>'''
 
 if __name__ == '__main__':
     app.run()
