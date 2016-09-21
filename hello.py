d2fa8a379411823419f28fee00c8f58533b87a3b
Seiji Yawata <seijiyawata@gmail.com>
2016-09-20 20:07:14 -0400

    Add styling

--- hello.py
+++ hello.py
@@ -4,23 +4,26 @@ import random
 # New Flask object 
 app = Flask(__name__)
 
+STYLE = "<style>body{margin:1em auto;max-width:40em;padding:0 .62em;font:1.2em/1.62em sans-serif;}h1,h2,h3{line-height:1.2em;}@media print{body{max-width:none}}</style>\n"
+
 # Associate route to fxn
 @app.route("/")
 def hola():
-    return '''<h1>Welcome to the Main Page!</h1><hr>
+    return STYLE + '''<h1>Welcome to the Main Page!</h1><hr>
 <a href=about>About</a><br>
 <a href=random>Random</a>
 '''
 
 @app.route("/about")
 def about():
-    return '''Made with Flask<hr>
+    return STYLE + '''Made with Flask<br>
+By Seiji Yawata <hr>
 <a href=..>Go Back</a>'''
 
 @app.route("/random")
 def info():
-    dice = random.randin(1,6)
-    return "You rolled a " + str(dice) + '''<hr>
+    dice = random.randint(1,6)
+    return STYLE + "You rolled a " + str(dice) + '''<hr>
 <a href=..>Go Back</a>'''
 
 if __name__ == '__main__':
